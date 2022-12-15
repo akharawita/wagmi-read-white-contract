c18d79ab30e4086578b480133b9584129db7ec67
Akharawit.A <akharawit.aryakom@gmail.com>
2022-12-15 08:17:20 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  useAccount,
+  useContractRead,
+  usePrepareContractWrite,
+  useContractWrite,
+} from "wagmi";
+import ABI from "./abis/ABI.json";
+
+import Profile from "./Profile";
+import { ethers } from "ethers";
 
 function App() {
+  const { address, isConnected } = useAccount();
+
+  // Read Contract
+  const { data } = useContractRead({
+    address: "0x9c44e4f71dc14c63F2cd7eA062Db9D60e2Da403d",
+    abi: ABI,
+    functionName: "getPositions",
+    args: [address],
+    enabled: isConnected,
+    watch: true,
+  });
+
+  // White Contract
+  const { config } = usePrepareContractWrite({
+    address: "0x9c44e4f71dc14c63F2cd7eA062Db9D60e2Da403d",
+    abi: ABI,
+    functionName: "openPosition",
+    args: [0, 100000000],
+    enabled: isConnected,
+  });
+  const { write, isLoading: whiteIsLoading } = useContractWrite(config);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <Profile />
+        </div>
+
+        <button disabled={!write} onClick={() => write?.()}>
+          Open Position{whiteIsLoading ? "...." : null}
+        </button>
+
+        <div>Positions</div>
+        <div>
+          {data &&
+            data.map((d) => (
+              <div key={d[0]}>
+                <div>Amount: {ethers.utils.formatUnits(d.amount, 8)}</div>
+                <div>Price: {ethers.utils.formatUnits(d.entryPrice, 8)}</div>
+                <hr />
+              </div>
+            ))}
+        </div>
       </header>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,13 +1,43 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import { WagmiConfig, createClient, configureChains } from "wagmi";
+import { jsonRpcProvider } from "wagmi/providers/jsonRpc";
+import { bscTestnet } from "wagmi/chains";
+
+const { provider, webSocketProvider } = configureChains(
+  [bscTestnet],
+  [
+    jsonRpcProvider({
+      rpc: (chain) => {
+        if (chain === 97) {
+          return {
+            http: "https://data-seed-prebsc-1-s1.binance.org:8545/",
+          };
+        }
+        return {
+          http: "https://data-seed-prebsc-2-s2.binance.org:8545/",
+        };
+      },
+    }),
+  ]
+);
+
+const client = createClient({
+  autoConnect: true,
+  provider,
+  webSocketProvider,
+});
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <WagmiConfig client={client}>
+      <App />
+    </WagmiConfig>
   </React.StrictMode>
 );
 
